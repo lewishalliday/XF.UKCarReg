b2a185e7bbcb6bec44d78a4101e2778a45525e60
Lewis Halliday <lewishalliday@gmail.com>
2020-05-26 08:12:31 +0100

    initial commit

---
+++ XF.GBCarReg.Android/Controls/VrmEntryRenderer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace XF.GBCarReg.Droid.Controls
+{
+    public class VrmEntryRenderer : ContentPage
+    {
+        public VrmEntryRenderer()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
